36571640cf36ba55739199ef84fc0d41503e5d28
Rig3 <olesya092006@yandex.ru>
2025-07-17 11:42:15 +0300

    Creating About

--- app/src/main/java/com/hfad/antiplag/presentation/about/AboutScreen.kt
+++ app/src/main/java/com/hfad/antiplag/presentation/about/AboutScreen.kt
@@ -1,15 +1,25 @@
 package com.hfad.antiplag.presentation.about
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.hfad.antiplag.R
 import com.hfad.antiplag.navigation.HomeRoute
+import com.hfad.antiplag.presentation.components.aboutInformation.AboutInformation
 import com.hfad.antiplag.presentation.components.customScaffold.CustomScaffold
 
 @Composable
@@ -37,6 +47,43 @@ fun AboutScreen(navController: NavHostController) {
 
         },
     ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Color.White
+                )
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "AntiPlag",
+                modifier = Modifier.padding(bottom = 10.dp),
+                color = Color.Black,
+                fontSize = 32.sp
+            )
+            AboutInformation(
+                image = R.drawable.rocket,
+                title = "Скорость",
+                text = "мгновенный анализ текста на заимствования."
+            )
+            AboutInformation(
+                image = R.drawable.target,
+                title = "Точность",
+                text = "сравнение с миллионами источников, включая академические работы и веб-страницы."
+            )
+            AboutInformation(
+                image = R.drawable.telephone,
+                title = "Простота использования",
+                text = "загружай текст или документ в один клик."
+            )
+            AboutInformation(
+                image = R.drawable.people,
+                title = "Для всех",
+                text = "студенты, преподаватели, копирайтеры, юристы."
+            )
+
+        }
 
     }
 
---
+++ app/src/main/java/com/hfad/antiplag/presentation/components/aboutInformation/AboutInformation.kt
@@ -0,0 +1,53 @@
+package com.hfad.antiplag.presentation.components.aboutInformation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.hfad.antiplag.R
+
+@Composable
+fun AboutInformation(
+    image: Int,
+    title: String,
+    text: String
+    ) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(10.dp),
+
+    ) {
+        Image(
+            painter = painterResource(
+                id = image
+            ),
+            contentDescription = null,
+            modifier = Modifier.size(53.dp).padding(10.dp)
+        )
+        Column (
+        ){
+            Text(
+                text = title,
+                color = Color.Black,
+                fontSize = 23.sp
+            )
+            Text(
+                text = text,
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+        }
+    }
+}
